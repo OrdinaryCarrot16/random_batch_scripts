7f0f9e388561fb1db27d0e8608c4f4a8cdd872f6
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2024-06-20 15:01:21 -0700

    Update Simple FFMpeg Edit And Re-Encode.bat
    Forgot to zero out the timestamps

--- Simple FFMpeg Edit And Re-Encode.bat
+++ Simple FFMpeg Edit And Re-Encode.bat
@@ -1,1 +1,1 @@
-ffmpeg -ss 00:00:01.500 -to 00:00:31.000 -i "FILE_IN.mkv" -c:v libvpx-vp9 -crf 20 -cpu-used 2 -row-mt 1 -c:a libopus -b:a 160k "FILE_OUT.webm"
+ffmpeg -ss 00:00:00.000 -to 00:00:00.000 -i "FILE_IN.mkv" -c:v libvpx-vp9 -crf 20 -cpu-used 2 -row-mt 1 -c:a libopus -b:a 160k "FILE_OUT.webm"
